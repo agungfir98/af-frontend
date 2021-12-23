065df8bd11223305613a637d3a275a06d04570f7
Agung Firmansyah <agungfir98@gmail.com>
2021-12-23 20:22:58 +0700

    remove skill section

--- pages/component/skill.tsx
+++ pages/component/skill.tsx
@@ -23,20 +23,5 @@ const array = [
 ];
 
 export default function Skill() {
-  return (
-    <div className="h-screen">
-      <h1>HALO</h1>
-      <div className="container px-4 mx-auto ">
-        <div className="flex flex-wrap mx-auto justify-">
-          <div className="w-24 h-24 bg-red-300 m-3"></div>
-          <div className="w-24 h-24 bg-red-300 m-3"></div>
-          <div className="w-24 h-24 bg-red-300 m-3"></div>
-          <div className="w-24 h-24 bg-red-300 m-3"></div>
-          <div className="w-24 h-24 bg-red-300 m-3"></div>
-          <div className="w-24 h-24 bg-red-300 m-3"></div>
-          <div className="w-24 h-24 bg-red-300 m-3"></div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="h-screen"></div>;
 }

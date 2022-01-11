27f6bb616b37d1f751c66a42daa26b82ae410bdc
Agung Firmansyah <agungfir98@gmail.com>
2022-01-11 15:56:06 +0700

    delete delete asset file, modif project font dan menambah kolom skills

--- pages/component/projects.tsx
+++ pages/component/projects.tsx
@@ -6,15 +6,20 @@ export default function Projects() {
   return (
     <div className="relative" id="projects">
       <div className="sm:pt-20 pt-16 container lg:px-32 md:px-16 px-4 mx-auto md:text-left">
-        <h1 className=" text-2xl sm:text-5xl font-bold">Projects</h1>
+        <h1 className="text-gray-800 dark:text-white-wdew text-2xl sm:text-5xl font-bold">
+          Projects
+        </h1>
         <div className="py-4">
           {data.map((res) => {
             return (
               <div key={res.title} className="py-2">
-                <h1 className="font-semibold">
+                <h1 className="text-xl font-semibold">
                   <li>{res.title}</li>
                 </h1>
-                <a href={res.url} className="flex px-6 text-white-blue2">
+                <a
+                  href={res.url}
+                  className="w-32 flex px-8 text-md pt-1 text-red-500"
+                >
                   {res.platform}
                   <ExternalLinkIcon className="h-3 w-3" />
                 </a>
